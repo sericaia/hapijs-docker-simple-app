f554361f665871f07afe20e9aa914c8175fa5b69
Daniela <alunassertiva@gmail.com>
2017-03-03 14:05:35 +0000

    fix docker file

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var Hapi = require('hapi');
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
-    port: 8080
+    host: '0.0.0.0', 
+    port: 8000
 });
 
 // Add the route
@@ -17,4 +17,10 @@ server.route({
 });
 
 // Start the server
-server.start();
+server.start(function(err) {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
